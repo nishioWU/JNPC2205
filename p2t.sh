0143f4c32167048fd46cecef2470e63acfc688b8
nishioWU <yoshito.nishio+WU@gmail.com>
2022-05-11 02:19:31 +0900

    Delete p2t.sh

--- p2t.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-find . -name '*.pdf' | xargs -I{} dirname {} | sed s/^.// | xargs -I{} mkdir -p extracted_text{}
-find . -name '*.pdf' | xargs -I{} pdftotext -nopgbrk {}
-find . -name '*.pdf' | sed s/^.// | sed s/.pdf$/.txt/ | xargs -I{} mv .{} extracted_text{}
