757cbb22ea97a8dd2cbcc2a18e02dab01788fea3
nishioWU <yoshito.nishio+WU@gmail.com>
2022-05-11 02:18:15 +0900

    シェルスクリプト
    サンプルのプログラム。エラー処理などは入れていない最小限のもの。

---
+++ p2t.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+find . -name '*.pdf' | xargs -I{} dirname {} | sed s/^.// | xargs -I{} mkdir -p extracted_text{}
+find . -name '*.pdf' | xargs -I{} pdftotext -nopgbrk {}
+find . -name '*.pdf' | sed s/^.// | sed s/.pdf$/.txt/ | xargs -I{} mv .{} extracted_text{}
