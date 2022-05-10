214144d5624beb70586cbf196b0e8079b388a499
nishioWU <yoshito.nishio+WU@gmail.com>
2022-05-11 02:20:07 +0900

    Delete SAMPLE directory

--- SAMPLE/p2t.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-find . -name '*.pdf' | xargs -I{} dirname {} | sed s/^.// | xargs -I{} mkdir -p OUTPUT{}
-find . -name '*.pdf' | xargs -I{} pdftotext -nopgbrk {}
-find . -name '*.pdf' | sed s/^.// | sed s/.pdf$/.txt/ | xargs -I{} mv .{} OUTPUT{}
