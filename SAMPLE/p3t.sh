2cb0618eea1c240ad372e209a34824996af3f833
nishioWU <yoshito.nishio+WU@gmail.com>
2022-05-13 01:07:11 +0900

    sedのコマンドを''でクオートした
    動作は変わらない。見やすくするためと、改変時の安全のため

--- SAMPLE/p3t.sh
+++ SAMPLE/p3t.sh
@@ -3,6 +3,6 @@
 # 出力先フォルダにタイムスタンプを付加した例
 dir_prefix="extracted_text_"
 dir_full="$dir_prefix`date +%Y-%m-%d_%H:%M:%S`"
-find . -name '*.pdf' | xargs -I{} dirname {} | sed s/^.// | xargs -I{} mkdir -p "$dir_full"{}
+find . -name '*.pdf' | xargs -I{} dirname {} | sed 's/^.//' | xargs -I{} mkdir -p "$dir_full"{}
 find . -name '*.pdf' | xargs -I{} pdftotext -nopgbrk {}
-find . -name '*.pdf' | sed s/^.// | sed s/.pdf$/.txt/ | xargs -I{} mv .{} "$dir_full"{}
+find . -name '*.pdf' | sed 's/^.//' | sed 's/.pdf$/.txt/' | xargs -I{} mv .{} "$dir_full"{}
